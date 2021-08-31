00dc59ec6dc437bec53349e6be5a69eb57dddb93
Zuzanna Piecowska <zsochocka@icloud.com>
2021-08-31 16:00:03 +0200

    FirstTest

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,17 +1,27 @@
 package com.kodilla.testing;
-import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.user.Calculator;
 
 public class TestingMain {
 
-        public static void main(String[] args) {
-            SimpleUser simpleUser = new SimpleUser("theForumUser");
+    public static void main(String[] args) {
 
-            String result = simpleUser.getUsername();
+        Calculator calculator = new Calculator();
 
-            if (result.equals("theForumUser")) {
-                System.out.println("test OK");
-            } else {
-                System.out.println("Error!");
+        int result = calculator.add(6, 5);
+        int result1 = calculator.substract(6, 5);
+
+        if (result == 11) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Test ERROR");
+        }
+
+        if (result1 == 1) {
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Test ERROR");
+
             }
         }
-}
+    }
+
--- kodilla-testing/src/main/java/com/kodilla/testing/user/Calculator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/user/Calculator.java
@@ -1,2 +1,16 @@
-package com.kodilla.testing.user;public class Calculator {
+package com.kodilla.testing.user;
+
+public class Calculator {
+
+    int a = 6;
+
+    int b = 5;
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int substract(int a, int b) {
+        return a - b;
+    }
 }
