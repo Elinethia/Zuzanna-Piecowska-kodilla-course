3b03c189314cef4a92e0785979084e35e15cc9c7
Zuzanna Piecowska <zsochocka@icloud.com>
2021-09-03 08:47:03 +0200

    Merge branch 'main' of https://github.com/Elinethia/Zuzanna-Piecowska-kodilla-course
    # Conflicts:
    #	kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -1,2 +1,21 @@
-package com.kodilla.testing.collection;public class OddNumbersExterminator {
+package com.kodilla.testing.collection;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class OddNumbersExterminator {
+
+    public List<Integer> exterminate(List<Integer> numbers) {
+
+        List<Integer> oddNumbersList = new ArrayList<>();
+
+        for(int num : numbers) {
+            if (num % 2 == 0) {
+                oddNumbersList.add(num);
+            }
+
+        }
+            return oddNumbersList;
+    }
+
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,2 +1,72 @@
-package com.kodilla.testing.collection;public class CollectionTestSuite {
+package com.kodilla.testing.collection;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import com.kodilla.testing.collection.OddNumbersExterminator;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class CollectionTestSuite {
+
+   OddNumbersExterminator exterminator = new OddNumbersExterminator();
+
+       @DisplayName(
+                "checking behavior when list is empty"
+        )
+
+        @Test
+        public void testOddNumbersExterminatorEmptyList() {
+
+
+            //Arrange
+            List<Integer> eList = new ArrayList<>();
+
+            //Act
+            List<Integer> rList = exterminator.exterminate(eList);
+
+            System.out.println("Test");
+
+            //Assert
+            Assertions.assertEquals(eList, rList);
+        }
+
+
+        @DisplayName(
+                "checking behavior when list is full"
+        )
+
+        @Test
+        public void testOddNumbersExterminatorNormalsList() {
+
+            //Arrange
+           List<Integer> fList = new ArrayList<>();
+
+            fList.add(4);
+            fList.add(7);
+            fList.add(21);
+            fList.add(29);
+            fList.add(5);
+            fList.add(13);
+            fList.add(16);
+            fList.add(9);
+            fList.add(20);
+            fList.add(12);
+
+            //Act
+            List<Integer> r1List = exterminator.exterminate(fList);
+
+            System.out.println("Test");
+
+            //Assert
+            Assertions.assertEquals(fList, r1List);
+
+        }
+
 }
+
+
